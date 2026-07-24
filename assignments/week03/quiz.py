214e512719b21562eb1586122c373ceb402cfff8
Nalintorn Sopapiriyatorn <nalintorn.s@ku.th>
2026-07-24 09:58:13 +0700

    if-elif-else

--- assignments/week03/quiz.py
+++ assignments/week03/quiz.py
@@ -7,8 +7,16 @@ age = int(input("Enter age: "))
 # 20-59: Adult
 # 60+: Senior
 
-# Your code here:
-
+if age >= 0 and age <= 12:
+    print("Child")
+elif age >= 13 and age <= 19:
+    print("Teenager")
+elif age >= 20 and age <= 59:
+    print("Adult")
+elif age >= 60:
+    print("Senior")
+else:
+    print("Invalid age")
 
 
 # Complete this ATM simulation
